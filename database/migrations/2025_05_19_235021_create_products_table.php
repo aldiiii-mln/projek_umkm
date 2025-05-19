a295cb8752635b606562be4096f234d29bda51e3
Muhammad Aldi Maulana <aldiialdi60@gmail.com>
2025-05-20 07:54:00 +0800

    penambahan field/atribut di semua file migration

--- database/migrations/2025_05_19_235021_create_products_table.php
+++ database/migrations/2025_05_19_235021_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('quantity');
+            $table->float('price');
+            $table->string('image')->nullable();
+            $table->enum('status', ['ada','tidak ada'])->default('ada');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2025_05_19_235031_create_kategoris_table.php
+++ database/migrations/2025_05_19_235031_create_kategoris_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->enum('status', ['ada','habis'])->default('ada');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
